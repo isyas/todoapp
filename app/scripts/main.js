cb0930d7e5eb470a2a975625260e1fd9e6554774
石井允康 <ishii-m@ishiimakotoyasushinoMacBook-Pro.local>
2017-05-11 20:31:02 +0900

    top 完成

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,48 @@
-console.log('\'Allo \'Allo!');
+// class App {
+//   constructor() {
+//     this.counter = 0;
+//   }
+//
+//   start() {
+//     // var self = this;
+//     // document.addEventListener('DOMContentLoaded', function(){
+//     //   console.log('DOMContentLoaded');
+//     //   self._setEvent();
+//     // });
+//
+//     document.addEventListener('DOMContentLoaded', () => {
+//       // console.log('DOMContentLoaded');
+//       // console.log(this); // App{}
+//       this._setEvent();
+//     });
+//
+//   }
+//
+//   _setEvent() {
+//     document.getElementById('push-btn').addEventListener('click', () => {
+//       // console.log('ボタンが押された！！');
+//       this.counter++;
+//       var txt = document.getElementById('input-text').value;
+//       var pEl = this._createElem(txt);
+//       this._outputElem(pEl);
+//       console.log(this);
+//     }, false);
+//   }
+//
+//   _createElem(txt) {
+//     var pEl = document.createElement('p');
+//     // ES5
+//     // pEl.textContent = txt;
+//     // ES2016
+//     pEl.textContent = `${this.counter} : ${txt}`;
+//
+//     return pEl;
+//   }
+//
+//   _outputElem(el) {
+//     document.getElementById('output').appendChild(el);
+//   }
+// }
+//
+// var app = new App();
+// app.start();
